7f8ff7ac0f57cc03d8141cc3617c0b725e5137ed
Randy Morales <randymoralesg@gmail.com>
2016-04-28 23:14:16 -0600

    Update pruebavers1.asm

--- pruebavers1.asm
+++ pruebavers1.asm
@@ -7,38 +7,65 @@ section .bss
 	buffEtiquetaLen equ 1024
 	bEtiqueta resb buffEtiquetaLen
 	
-	
-
 section .text
-
 global _start
 
-					_start:
+_start:
 ; CONTADOR QUE PASA POR LOS 4K DEL DOCUMENTO ES.... r15
-
 
 	leerDocumentos documento, bufflen   ;macro que lee el documento
 	cmp rax, 0
 	je salir	
-	mov rbp, rax
-
+	mov rbp, rax	;RBP guarda el tamaño del documento
 	xor r15, r15
 
-BuscaMenorQue:
+BuscaMenorQue:	;busca el inicio de la etiqueta (el <)
 	cmp[documento + r15], byte 0x3C ;busca < en el documento
 	je BuscarEspacio
+	cmp[documento + r15], byte 0h
+	je imprimir
 	inc r15
 	jmp BuscaMenorQue
 
-	 
+BuscarEspacio:	;busca el final de la etiqueta
+	push rcx
+	.buscarEspacioYGuardar:	;guardar toda la etiqueta
+		; r14 CONTADOR PARA EL BUfFER
+		xor rcx, rcx
+		cmp[documento + r15], byte 20h
+		je .hacerMatch
+		
+		mov rcx, [documento + r15]
+		mov [bEtiqueta + r14], rcx
+		inc r14
+		inc r15
+		cmp [documento + r15-1], byte 3Eh
+		je .retornar
+		jmp .buscarEspacioYGuardar
 
-	;.BuscaMayorQue:
-	;	  cmp [documento + r15],0x3E
-	;	  je funcion de buscar el otro
-	;	  inc r15
-	;	  jmp .analisis
+	.hacerMatch: ;buscar el > del tag de cierre 
+		cmp [documento + r15], byte 3Eh	; >
+		je .salvarCaracter
+		cmp [documento + r15], byte 3Ch	; <
+		je .retornar
+		inc r15
+		jmp .hacerMatch
 
+	.salvarCaracter:
+		mov rcx, [documento + r15]
+		mov [bEtiqueta + r14], rcx
+		inc r14
+		inc r15
+		jmp .retornar
 
+	.retornar:
+		pop rcx
+		;mov [bEtiqueta + r14], byte 3Eh
+		;inc r14
+		cmp r15, rbp
+		jb	BuscaMenorQue
+
+;*****************************************************************************************************
 imprimir:
 	mov rax, 1
 	mov rdi, 1
@@ -46,43 +73,7 @@ imprimir:
 	mov rdx, r14
 	syscall
 
-
 salir:
 	mov rax, 60
 	mov rdi, 0
 	syscall
-
-
-
-BuscarEspacio:
-	push r15
-	push rcx
-
-	.buscarEspacioYGuardar:	
-			; r14 CONTADOR PARA EL BUfFER
-		xor rcx, rcx
-		cmp[documento + r15], byte 20h
-		je .retornar
-		mov rcx, [documento + r15]
-		mov [bEtiqueta + r14], rcx
-		inc r14
-		inc r15
-		jmp .buscarEspacioYGuardar
-
-	.retornar:
-		pop rcx
-		pop r15
-		jmp imprimir
-	
-
-
-
-
-
-
-
-
-
-
-
-
